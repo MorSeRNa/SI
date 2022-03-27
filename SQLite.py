43614673ac7f192be30206653e5410f445912da7
Sergio Mora <morserna1@gmail.com>
2022-03-27 16:10:16 +0200

    Add Exercise 3

--- SQLite.py
+++ SQLite.py
@@ -100,9 +100,9 @@ def valorminmax(df, column):
                 lista.append(0)
     return str(np.amin(np.array(lista)))+"/"+str(np.amax(np.array(lista)))
 
-def valores_vacios(dataframe):
+def valores_vacios(df):
     vacios = 0
-    for index, row in dataframe.iterrows():
+    for index, row in df.iterrows():
         if row["telefono"] == "None":
             vacios += 1
         if row["provincia"] == "None":
@@ -113,7 +113,60 @@ def valores_vacios(dataframe):
             vacios += 1
     return vacios
 
-def print_ejercicio1(df):
+
+
+
+
+
+
+
+
+def observaciones(df):
+    sum = 0
+    for index, row in df.iterrows():
+        sum+=literal_eval(row["emails"])["phishing"]
+    return sum
+
+
+def valores_vacios_3(df):
+    vacios = 0
+    for index, row in df.iterrows():
+        if literal_eval(row["emails"])["phishing"] is None or math.isnan(literal_eval(row["emails"])["phishing"]):
+            vacios += 1
+    return vacios
+
+def mediana(df):
+    lista = []
+    for index, row in df.iterrows():
+            if row["emails"] != "None":
+                    lista.append(literal_eval(row["emails"])["phishing"])
+            else:
+                lista.append(0)
+    return np.median(lista)
+def media(df):
+    sum = 0
+    for index, row in df.iterrows():
+            if row["emails"] != "None":
+                    sum+=literal_eval(row["emails"])["phishing"]
+    return sum/len(df)
+def varianza(df):
+    lista = []
+    for index, row in df.iterrows():
+            if row["emails"] != "None":
+                    lista.append(literal_eval(row["emails"])["phishing"])
+            else:
+                lista.append(0)
+    return np.var(lista)
+
+def valorminmax_3(df):
+    lista = []
+    for index, row in df.iterrows():
+            if row["emails"] != "None":
+                    lista.append(literal_eval(row["emails"])["phishing"])
+            else:
+                lista.append(0)
+    return str(np.amin(np.array(lista)))+"/"+str(np.amax(np.array(lista)))
+def print_ejercicio2(df):
     print("Total de muestras: " +str(len(df) * len(df.columns) - valores_vacios(df)))
     print("Media fechas : " +str(calcular_media(df, "fechas")))
     print("Desviacion estandar fechas: " +str(desviacion_estandar(df, "fechas")))
@@ -125,6 +178,45 @@ def print_ejercicio1(df):
 
     print("Valor minimo / maximo de fechas que han iniciado sesion : " +valorminmax(df, "fechas"))
     print("Valor minimo / maximo del numero de emails recibidos: " +valorminmax(df, "emails"))
+
+def agrupacion_rol(con, permisos):
+    df = pd.read_sql_query("SELECT * from usuarios WHERE permisos = "+str(permisos), con)
+    estadisticas_ej3(df)
+
+def agrupacion_correos(con, cantidad):
+    df = pd.read_sql_query("SELECT * from usuarios", con)
+    for index, row in df.iterrows():
+        if(cantidad == 200):
+            if(literal_eval(row["emails"])["total"] < 200):
+                df = df.drop(index)
+        elif(cantidad == -200):
+            if(literal_eval(row["emails"])["total"] >= 200):
+                df = df.drop(index)
+    estadisticas_ej3(df)
+
+
+def estadisticas_ej3(df):
+    print("Número de observaciones:", observaciones(df))
+    print("Número de valores ausentes:", valores_vacios_3(df))
+    print("Mediana:", mediana(df))
+    print("Media:", media(df))
+    print("Varianza:", varianza(df))
+    print("Valor mínimo/máximo:", valorminmax_3(df))    
+
+def print_ejercicio3(con, df):
+   
+    print("Agrupacion por Usuario")
+    agrupacion_rol(con, 0)
+    print("-----------------------")
+    print("Agrupacion por Administrador")
+    agrupacion_rol(con, 1)
+
+    print("Agrupacion por +200 correos")
+    agrupacion_correos(con, 200)
+    print("Agrupacion por -200 correos")
+    agrupacion_correos(con, -200)
+
+    
    
 
 
@@ -143,6 +235,8 @@ con = sqlite3.connect('example.db')
 
 df_legal = sql_show_table(con, "legal")
 df_usuarios = sql_show_table(con, "usuarios")
-print_ejercicio1(df_usuarios)
+
+#print_ejercicio2(df_usuarios)
+print_ejercicio3(con, df_usuarios)
 
 con.close()
