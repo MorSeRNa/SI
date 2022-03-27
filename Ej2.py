efb42c51075bf22f891c8e4ff8f7c203d070330c
Sergio Mora <morserna1@gmail.com>
2022-03-27 16:16:37 +0200

    Separate exercises into different files

--- Ej2.py
+++ Ej2.py
@@ -113,59 +113,6 @@ def valores_vacios(df):
             vacios += 1
     return vacios
 
-
-
-
-
-
-
-
-
-def observaciones(df):
-    sum = 0
-    for index, row in df.iterrows():
-        sum+=literal_eval(row["emails"])["phishing"]
-    return sum
-
-
-def valores_vacios_3(df):
-    vacios = 0
-    for index, row in df.iterrows():
-        if literal_eval(row["emails"])["phishing"] is None or math.isnan(literal_eval(row["emails"])["phishing"]):
-            vacios += 1
-    return vacios
-
-def mediana(df):
-    lista = []
-    for index, row in df.iterrows():
-            if row["emails"] != "None":
-                    lista.append(literal_eval(row["emails"])["phishing"])
-            else:
-                lista.append(0)
-    return np.median(lista)
-def media(df):
-    sum = 0
-    for index, row in df.iterrows():
-            if row["emails"] != "None":
-                    sum+=literal_eval(row["emails"])["phishing"]
-    return sum/len(df)
-def varianza(df):
-    lista = []
-    for index, row in df.iterrows():
-            if row["emails"] != "None":
-                    lista.append(literal_eval(row["emails"])["phishing"])
-            else:
-                lista.append(0)
-    return np.var(lista)
-
-def valorminmax_3(df):
-    lista = []
-    for index, row in df.iterrows():
-            if row["emails"] != "None":
-                    lista.append(literal_eval(row["emails"])["phishing"])
-            else:
-                lista.append(0)
-    return str(np.amin(np.array(lista)))+"/"+str(np.amax(np.array(lista)))
 def print_ejercicio2(df):
     print("Total de muestras: " +str(len(df) * len(df.columns) - valores_vacios(df)))
     print("Media fechas : " +str(calcular_media(df, "fechas")))
@@ -178,46 +125,6 @@ def print_ejercicio2(df):
 
     print("Valor minimo / maximo de fechas que han iniciado sesion : " +valorminmax(df, "fechas"))
     print("Valor minimo / maximo del numero de emails recibidos: " +valorminmax(df, "emails"))
-
-def agrupacion_rol(con, permisos):
-    df = pd.read_sql_query("SELECT * from usuarios WHERE permisos = "+str(permisos), con)
-    estadisticas_ej3(df)
-
-def agrupacion_correos(con, cantidad):
-    df = pd.read_sql_query("SELECT * from usuarios", con)
-    for index, row in df.iterrows():
-        if(cantidad == 200):
-            if(literal_eval(row["emails"])["total"] < 200):
-                df = df.drop(index)
-        elif(cantidad == -200):
-            if(literal_eval(row["emails"])["total"] >= 200):
-                df = df.drop(index)
-    estadisticas_ej3(df)
-
-
-def estadisticas_ej3(df):
-    print("Número de observaciones:", observaciones(df))
-    print("Número de valores ausentes:", valores_vacios_3(df))
-    print("Mediana:", mediana(df))
-    print("Media:", media(df))
-    print("Varianza:", varianza(df))
-    print("Valor mínimo/máximo:", valorminmax_3(df))    
-
-def print_ejercicio3(con, df):
-   
-    print("Agrupacion por Usuario")
-    agrupacion_rol(con, 0)
-    print("-----------------------")
-    print("Agrupacion por Administrador")
-    agrupacion_rol(con, 1)
-
-    print("Agrupacion por +200 correos")
-    agrupacion_correos(con, 200)
-    print("Agrupacion por -200 correos")
-    agrupacion_correos(con, -200)
-
-    
-   
 
 
 
@@ -235,8 +142,6 @@ con = sqlite3.connect('example.db')
 
 df_legal = sql_show_table(con, "legal")
 df_usuarios = sql_show_table(con, "usuarios")
-
-#print_ejercicio2(df_usuarios)
-print_ejercicio3(con, df_usuarios)
+print_ejercicio2(df_usuarios)
 
 con.close()
