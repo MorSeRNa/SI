f18d864902ee75b3cf52b63542e62d077bfe36e3
Sergio Mora <morserna1@gmail.com>
2022-03-28 15:37:18 +0200

    Passwords debiles
    Creadas la funcion que recorre un dictionario y compara hashes con las del usuario para saber si es debil o no

--- Ej4.py
+++ Ej4.py
@@ -5,15 +5,29 @@ import statistics
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import hashlib
 
+def esPasswordSegura(password):
+    with open("10k-most-common.txt", encoding='utf8') as f:
+        for i in f.read().splitlines():
+            linea = hashlib.md5(bytes(i, encoding='utf8'))
+            if linea.hexdigest() == password:
+                return 1
+    return 0
 
-def usuarios_vulnerables():
+def usuarios_vulnerables(con):
     df = pd.read_sql_query("SELECT * from usuarios", con)
     for index, row in df.iterrows():
         if(literal_eval(row["emails"])["phishing"] != 0):
             df._set_value(index, "probabilidad", (literal_eval(row["emails"])["cliclados"]/literal_eval(row["emails"])["phishing"])*100)
         else:
             df._set_value(index, "probabilidad", 0)
+        df._set_value(index, "secure_pass", esPasswordSegura(row["contrasena"]))
+    
+    df = df[df["secure_pass"] == 0]
+    df = df.sort_values(["probabilidad"], ascending=[False])
+
+    print(df)
     df = df.head(10)
     plt.bar(np.arange(len(df)), df['probabilidad'], width=0.4, color='r', label='cookies')
     plt.xticks(np.arange(len(df))+0.4, df["id_usuario"])
@@ -49,10 +63,24 @@ def comparar_webs_creacion(con, ano):
     print(df[df["Politicas"] != 0])
     
 
+def comparar_passwords_comprometidas(con):
+    df = pd.read_sql_query("SELECT * from usuarios", con)
+    comprometidos = []
+    no_comprometidos = []
+    for index, row in df.iterrows():
+        if(esPasswordSegura(row["contrasena"])):
+            no_comprometidos.append(row)
+        else:
+            comprometidos.append(row)
+    print("Passwords comprometidas: "+str(len(comprometidos)))
+    print("Passwords no comprometidas: "+str(len(no_comprometidos)))
+    
+    
    
 
 con = sqlite3.connect('example.db')
-#usuarios_vulnerables(con)
-#politicas_desactualizadas(con)
+usuarios_vulnerables(con)
+politicas_desactualizadas(con)
 comparar_webs_creacion(con, 2001)
+comparar_passwords_comprometidas(con)
 con.close()
