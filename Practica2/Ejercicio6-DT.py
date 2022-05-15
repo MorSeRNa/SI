4198895cdada076300c661d0e9e2e130981ab6d5
Sergio Mora <morserna1@gmail.com>
2022-05-15 23:23:20 +0200

    Issues fixed

--- Practica2/Ejercicio6-DT.py
+++ Practica2/Ejercicio6-DT.py
@@ -1,18 +1,11 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import json
-from sklearn import linear_model
-from sklearn.metrics import mean_squared_error
 from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import export_graphviz
 import graphviz
 
 
 with open("users_IA_clases.json", "r") as file:
     data = json.load(file)
-
 
 df = pd.DataFrame(data['usuarios'])
 
@@ -21,11 +14,8 @@ for index, row in df.iterrows():
         df._set_value(index, "prob_click", 0)
     else:
         df._set_value(index, "prob_click", row["emails_phishing_clicados"] / row["emails_phishing_recibidos"])
-df_data = df.to_numpy()
 
 
-
-## Decisión tree ##
 X = df.iloc[:, 4].values
 X, y = X.reshape(-1,1), df["vulnerable"].to_numpy()
 clf = tree.DecisionTreeClassifier()
@@ -45,13 +35,7 @@ for index, row in df.iterrows():
     else:
         df._set_value(index, "prob_click", row["emails_phishing_clicados"] / row["emails_phishing_recibidos"])
 
-df_data = df.to_numpy()
-
 clf.predict(df["prob_click"].values.reshape(-1, 1))
-
-dot_data = tree.export_graphviz(clf, out_file=None)
-graph = graphviz.Source(dot_data)
-graph.render("decisiontree")
 dot_data = tree.export_graphviz(clf, out_file=None,filled=True, rounded=True,special_characters=True)
 graph = graphviz.Source(dot_data)
-graph.render('decisiontree.gv', view=True).replace('\\', '/')
+graph.render('decisiontree', view=True).replace('\\', '/')
